b795396da8cf202f0da9492e4edbe60c73e34bed
desertfoox <salar.nili097@gmail.com>
2021-12-20 20:30:58 +0330

    add header

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = (): JSX.Element => {
   return (
-    <div className="bg-gray-400 text-center fixed bottom-0 right-0 w-full">
+    <div className="bg-gray-400 text-center absolute bottom-0 right-0 w-full">
        تمامی حقوق این سایت متعلق به سالار نیلی میباشد 
     </div>
   );
--- src/Screens/Landing/Landing.tsx
+++ src/Screens/Landing/Landing.tsx
@@ -1,11 +1,13 @@
 import React from "react";
-import Footer from "../../Components/Footer/Footer";
 
+import Footer from "../../Components/Footer/Footer";
+import Header from "../../Components/Header/Header";
 import LandingContainer from "../../Components/LandingContainer/LandingContainer";
 
 const Landing = () => {
   return (
     <>
+      <Header />
       <LandingContainer />
       <Footer />
     </>
